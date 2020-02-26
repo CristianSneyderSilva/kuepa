6485d6cdacb7f1d5ecd48e227af1778a7c013d8c
cristiansilva <cristiansilva@cbit-online.com>
2020-02-26 17:06:48 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// #seccion de componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+// #fin seccion de componentes
+
+// Componentes sync
+import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+// #Fin componentes sync
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DateRangePickerModule,
+    DropDownListModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
